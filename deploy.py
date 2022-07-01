4b3703c0b7a20b08bac9ba50691a08f5fcb47ef0
Sohil Zidan <zidansohil@gmail.com>
2022-07-01 15:14:43 +0000

    maintain end2end model

--- deploy.py
+++ deploy.py
@@ -2,9 +2,9 @@ from google.cloud import aiplatform
 
 # Create a model resource from public model assets
 model = aiplatform.Model.upload(
-    display_name="opendataportal-imported",
+    display_name="mta-end2end",
     # artifact_uri="dps-opendataportal-354609-bucket/opendataportal/model",
-    artifact_uri="gs://dps-opendataportal-354609-bucket/opendataportal/model",
+    artifact_uri="gs://dps-opendataportal-354609-bucket/opendataportal/end2endmodel",
     # artifact_uri="gs://dps-opendataportal-354609-bucket/opendataportal-model",
     
     serving_container_image_uri="gcr.io/cloud-aiplatform/prediction/tf2-cpu.2-3:latest"
