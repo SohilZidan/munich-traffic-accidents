0c75c5f7a651ae0fc5a5f650c35bd8195b1cf081
Sohil Zidan <zidansohil@gmail.com>
2022-07-01 15:15:14 +0000

    isolate input transformation

---
+++ transform_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def transform(sample_raw):
+    sample = [
+        int(sample_raw[2]),
+        int(sample_raw[3]),
+        int(sample_raw[0] == "Alkoholunfälle"),
+        int(sample_raw[0] == "Fluchtunfälle"),
+        int(sample_raw[0] == "Verkehrsunfälle"),
+        int(sample_raw[1] == "Verletzte und Getötete"),
+        int(sample_raw[1] == "insgesamt"),
+        int(sample_raw[1] == "mit Personenschäden")
+    ]
+    return sample
