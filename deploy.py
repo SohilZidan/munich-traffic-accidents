9ac468915276d1cb9f1c1a5631a1d6a0d4616508
Sohil Zidane <zidansohil@gmail.com>
2023-11-21 20:11:19 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,13 +1,15 @@
 from google.cloud import aiplatform
 
+BUCKET = "gs://dps-challenge-354007-bucket"
+
 # Create a model resource from public model assets
 model = aiplatform.Model.upload(
-    display_name="mta-end2end",
+    display_name="mta-deployed",
     # artifact_uri="dps-opendataportal-354609-bucket/opendataportal/model",
-    artifact_uri="gs://dps-opendataportal-354609-bucket/opendataportal/end2endmodel",
+    artifact_uri=BUCKET+"/mta/end2endmodel",
     # artifact_uri="gs://dps-opendataportal-354609-bucket/opendataportal-model",
     
-    serving_container_image_uri="gcr.io/cloud-aiplatform/prediction/tf2-cpu.2-3:latest"
+    serving_container_image_uri="gcr.io/cloud-aiplatform/prediction/tf2-cpu.2-6:latest"
 )
 
 # Deploy the above model to an endpoint
